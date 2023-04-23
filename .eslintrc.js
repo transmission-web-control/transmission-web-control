ba5e6a9a54e0f030fe9cff3721467fa781a37b5d
Trim21 <trim21.me@gmail.com>
2023-04-24 03:08:36 +0800

    migrate plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: ['standard-with-typescript', 'prettier'],
   overrides: [],
   parserOptions: {
+    project: './tsconfig.json',
     ecmaVersion: 'latest',
   },
   rules: {
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
-/// <reference types="vite/client" />
+import 'vite/client';
+
+declare global {
+  const system: {
+    plugin: { exec(key: string): void };
+
+    control: any;
+    openDialogFromTemplate(config: {
+      id: string;
+      options: unknown;
+      datas?: unknown;
+      type?: number;
+      source?: unknown;
+    }): void;
+  };
+}
